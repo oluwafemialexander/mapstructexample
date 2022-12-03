d2f554966ab0a9a8a6908738f8efa4ecec774cd5
oluwafemialexander <80492571+oluwafemialexander@users.noreply.github.com>
2022-12-04 00:44:44 +0100

    First Commit

---
+++ src/main/java/com/femialex/mapstructexample/MapstructexampleApplication.java
@@ -0,0 +1,13 @@
+package com.femialex.mapstructexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapstructexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapstructexampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/femialex/mapstructexample/MapstructexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.femialex.mapstructexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapstructexampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
