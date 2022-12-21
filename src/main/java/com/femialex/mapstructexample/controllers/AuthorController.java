24c537821c2fd33b49132b24a1e0f660edc2ccbd
oluwafemialexander <80492571+oluwafemialexander@users.noreply.github.com>
2022-12-21 20:06:31 +0100

    Setting Up Controllers. #3

--- src/main/java/com/femialex/mapstructexample/controllers/AuthorController.java
+++ src/main/java/com/femialex/mapstructexample/controllers/AuthorController.java
@@ -4,18 +4,14 @@ import com.femialex.mapstructexample.mapstruct.mapper.MapStructMapper;
 import com.femialex.mapstructexample.mapstruct.model.AuthorAllDto;
 import com.femialex.mapstructexample.repositories.AuthorRepository;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
 @RestController
 @RequestMapping("/authors")
-@Controller
 public class AuthorController {
 
     private final MapStructMapper mapStructMapper;
